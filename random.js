c5f813f9acb29309a453c6ba2d2fb72e1fdb14b5
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-02-17 20:03:24 +0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,20 @@
+var pages = [ 
+
+'https://bebastagihan.blogspot.com/",
+'https://blangkonkuota.blogspot.com/",
+'https://saldoppob.blogspot.com/",
+'https://solawatpulsa.blogspot.com/",
+'https://travelppob.blogspot.com/",
+'https://bebastagihan.blogspot.com/",
+'https://lapakrecehan.blogspot.com/",
+'https://tawontube.blogspot.com/",
+'https://tempatagenpulsa.blogspot.com/",
+'https://webbayar.blogspot.com/",
+
+
+
+]; 
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
