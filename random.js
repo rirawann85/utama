13406cc125721d04bce74881c59901b0c83cc52d
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-02-17 20:04:02 +0700

    Update random.js

--- random.js
+++ random.js
@@ -1,15 +1,17 @@
 var pages = [ 
 
-'https://bebastagihan.blogspot.com/",
-'https://blangkonkuota.blogspot.com/",
-'https://saldoppob.blogspot.com/",
-'https://solawatpulsa.blogspot.com/",
-'https://travelppob.blogspot.com/",
-'https://bebastagihan.blogspot.com/",
-'https://lapakrecehan.blogspot.com/",
-'https://tawontube.blogspot.com/",
-'https://tempatagenpulsa.blogspot.com/",
-'https://webbayar.blogspot.com/",
+"https://bebastagihan.blogspot.com/",
+"https://blangkonkuota.blogspot.com/",
+"https://saldoppob.blogspot.com/",
+"https://solawatpulsa.blogspot.com/",
+"https://travelppob.blogspot.com/",
+"https://bebastagihan.blogspot.com/",
+"https://lapakrecehan.blogspot.com/",
+"https://tawontube.blogspot.com/",
+"https://tempatagenpulsa.blogspot.com/",
+"https://webbayar.blogspot.com/",
+
+
 
 
 
